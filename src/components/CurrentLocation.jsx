710122e3d9e45fc4fb9012c6bb45e52e005ccb9c
Growcu <dawid.growiec@interia.pl>
2021-04-02 18:16:51 +0200

    added geolocation and make basics html structure

--- src/components/CurrentLocation.jsx
+++ src/components/CurrentLocation.jsx
@@ -1,8 +1,54 @@
-import React from 'react';
+/* eslint-disable no-useless-catch */
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-const CurrentLocation = () => (
-    <div className="currentLocation">
-        <h1>Aktualna lokalizacja</h1>
-    </div>
-);
+import { searchLocation } from '../actions/appActions';
+
+const CurrentLocation = () => {
+    const dispatch = useDispatch();
+    const weatherData = useSelector((store) => store.cityData);
+
+    const {
+        city, temp, pressure, humidity,
+    } = weatherData;
+
+    const getLocation = () => {
+        if ('geolocation' in navigator) {
+            try {
+                navigator.geolocation.getCurrentPosition((position) => {
+                    const location = {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                    };
+                    console.log('zapytanie');
+                    dispatch(searchLocation(location));
+                });
+            } catch (err) {
+                throw err;
+            }
+        } else {
+            alert('Brak mozliwosci lokalizacji');
+        }
+    };
+
+    useEffect(() => (
+        getLocation()
+    ), []);
+
+    return (
+        <div className="currentLocation">
+            <div className="weatherDataWrapper">
+                <ul className="weatherDataList">
+                    <li>{city}</li>
+                    <li>Time</li>
+                    <li>{temp}</li>
+                    <li>{pressure}</li>
+                    <li>{humidity}</li>
+                    <li>Ikona</li>
+                </ul>
+                <button type="button">More...</button>
+            </div>
+        </div>
+    );
+};
 export default CurrentLocation;
