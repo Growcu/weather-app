f42f183c544737bb10c3d69c285d44c9ef5f83f8
Growcu <dawid.growiec@interia.pl>
2021-04-02 18:15:51 +0200

    added request by geolocation

--- src/middlewares/downloadDataMiddleware.js
+++ src/middlewares/downloadDataMiddleware.js
@@ -1,23 +1,43 @@
 import axios from 'axios';
 
-const downloadDataMiddleware = () => (next) => async (action) => {
-    const { city } = action.payload;
+import { SEARCHCITY, SEARCHLOCATION } from '../actions/appActions';
 
-    await axios
-        .post(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=971d414e89f3c0b3df147fbb3ad30cb7`)
-        .then((response) => {
-            const {
-                temp,
-                pressure,
-                humidity,
-            } = response.data.main;
-            action.payload = {
-                city,
-                temp,
-                pressure,
-                humidity,
-            };
-        });
+const downloadDataMiddleware = () => (next) => async (action) => {
+    if (action.type === SEARCHLOCATION) {
+        const { latitude, longitude } = action.payload;
+        await axios
+            .post(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=971d414e89f3c0b3df147fbb3ad30cb7`)
+            .then((response) => {
+                const {
+                    temp,
+                    pressure,
+                    humidity,
+                } = response.data.main;
+                action.payload = {
+                    city: response.data.name,
+                    temp,
+                    pressure,
+                    humidity,
+                };
+            });
+    } else if (action.type === SEARCHCITY) {
+        const { city } = action.payload;
+        await axios
+            .post(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=971d414e89f3c0b3df147fbb3ad30cb7`)
+            .then((response) => {
+                const {
+                    temp,
+                    pressure,
+                    humidity,
+                } = response.data.main;
+                action.payload = {
+                    city,
+                    temp,
+                    pressure,
+                    humidity,
+                };
+            });
+    }
     next(action);
 };
 export default downloadDataMiddleware;
