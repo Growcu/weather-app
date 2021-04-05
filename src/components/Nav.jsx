13a44646c35a039f15c95ff0a134ca570834c64a
Growcu <dawid.growiec@interia.pl>
2021-04-05 14:24:57 +0200

    added style to app and nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import '../styles/Nav.scss';
+
 const Nav = () => (
     <nav className="nav">
-        <ul>
-            <li><a href="/CurrentLocation">Lokalizacja</a></li>
-            <li><a href="/OtherLocation">Inna miasto</a></li>
+        <ul className="nav_list">
+            <li><a href="/CurrentLocation" className="nav_item">Lokalizacja</a></li>
+            <li><a href="/OtherLocation" className="nav_item">Inna miasto</a></li>
         </ul>
     </nav>
 );
