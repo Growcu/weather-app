e85c67ad7f7556a80fe630e9f29f9abe46e0936d
Growcu <dawid.growiec@interia.pl>
2021-04-06 19:11:11 +0200

    improved time string

--- src/modules/timeReader.js
+++ src/modules/timeReader.js
@@ -1,6 +1,10 @@
 const timeReader = (time, timezone) => {
     const timeInMiliseconds = (time + timezone) * 1000;
     const currentTime = new Date(timeInMiliseconds);
-    return `${currentTime.getUTCHours()}:${currentTime.getUTCMinutes()}`;
+
+    const hours = currentTime.getUTCHours() <= 9 ? `0${currentTime.getUTCHours()}` : currentTime.getUTCHours();
+    const minutes = currentTime.getUTCMinutes() <= 9 ? `0${currentTime.getUTCMinutes()}` : currentTime.getUTCMinutes();
+
+    return `${hours}:${minutes}`;
 };
 export default timeReader;
