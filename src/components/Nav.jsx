3e979df7b97a6cdbe4ee9acd3393583bb7ad68bc
Growcu <dawid.growiec@interia.pl>
2021-04-07 09:24:38 +0200

    change href

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ import '../styles/Nav.scss';
 const Nav = () => (
     <nav className="nav">
         <ul className="nav_list">
-            <li><a href="/CurrentLocation" className="nav_item">Lokalizacja</a></li>
+            <li><a href="/" className="nav_item">Lokalizacja</a></li>
             <li><a href="/OtherLocation" className="nav_item">Inna miasto</a></li>
         </ul>
     </nav>
